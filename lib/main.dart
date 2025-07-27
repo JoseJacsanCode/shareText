90f541976244bfa583136854846a5c8bfd4393c3
Jose Jacsan <jjacsan@gmail.com>
2025-07-27 18:04:11 -0300

    Implementação do compartilhamento de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
+    return const MaterialApp(home: HomePage());
   }
 }
 
@@ -22,8 +21,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            shareButton();
+          },
+          child: Text(
+            'Clique aqui',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
 
+void shareButton() {
+  SharePlus.instance.share(
+    ShareParams(text: 'Testando a função do Share Plus'),
+  );
 }
